ad7620d9878ac33af3552fd0d133a96ed8c8c135
coleyoung-game <ighong11@gmail.com>
2025-09-08 13:59:57 +0900

    [Add] 모듈 내용 추가

---
+++ learn_python/module_1.py
@@ -0,0 +1,16 @@
+﻿def add(a, b):
+    return a + b
+
+def sub(a, b): 
+    return a-b
+
+# import시 바로 실행 하게 됨.
+# print(add(1, 4))
+# print(sub(4, 2))
+
+# __name__ 변수
+# 직접 .py 파일을 실행할 땐 내부의 __name__ 변수엔 __main__값이 저장된다.
+# 다른 파이썬 모듈에서 module_1을 import할 경우에는 __name__변수에 module_1이 저장된다.
+if __name__ == "__main__":
+    print(add(1, 4))
+    print(sub(4, 2))
---
+++ learn_python/module_main.py
@@ -0,0 +1,28 @@
+﻿from ast import IsNot
+import module_1
+# 모듈 이름, 모듈 함수
+from module_1 import add
+# 다중으로 불러올 수 있음
+from module_1 import add, sub
+# 모든 함수를 불러옴
+from module_1 import *
+
+#import module_2
+
+result = add(3,4)
+print(result)
+result = sub(5,2)
+print(result)
+
+
+# 다른 디렉터리에 있는 모듈 가져오기.
+import sys
+# 해당 구문을 실행하면 저장 된다. (다시 실행할 땐 실행하면 안될듯)
+sys.path.append("D:\Project\python\learn-python-git\learn_python\modules")
+print(sys.path)
+# 추가한 경로에 있는 module_2.py를 불러온다.
+import module_2
+result = module_2.add(3,4)
+
+print(result)
+
